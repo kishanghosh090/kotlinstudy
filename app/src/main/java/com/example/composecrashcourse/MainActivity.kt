56a3347634492474f15f6fadd736b19bc59e6128
kishanghosh090 <kishanghosh090@gamil.com>
2026-01-03 20:14:05 +0530

    add animation

--- app/src/main/java/com/example/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/composecrashcourse/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.composecrashcourse
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import com.example.composecrashcourse.ui.theme.ComposecrashcourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +47,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposecrashcourseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                    ){
-                        Text("hello from kishan ")
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    var isVisible by remember {
+                        mutableStateOf(false)
+                    }
+                    Button(
+                        onClick = {
+                            isVisible = !isVisible
+                        }
+                    ) {
+                        Text("Toggle")
+                    }
+                    AnimatedVisibility(
+                        visible = isVisible,
+                        modifier = Modifier.fillMaxWidth().weight(1f),
+                        enter =  slideInVertically() + fadeIn(),
+                        exit =  slideOutVertically() + fadeOut()
+
+                    ) {
+                            Box(modifier = Modifier.background(color = androidx.compose.ui.graphics.Color.Red)) { }
                     }
                 }
             }
